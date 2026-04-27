b4c096b8859dd8101f2d675babe8734a9ad3a6aa
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-27 11:36:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item1 = await createItem("hotwheels lamborghini", 39.99, 3);
 
 console.log(item2.subtotal());
+
+
+
+
+
+
+item.js
+// CASOS DE USO DOS ITENS
+
+
+//-> criar item com subtotal certo
+async function  createItem(name, price,quantity) {
+ return{
+    name,
+    price,
+    quantity,
+    subtotal:() => price * quantity,
+ };  
+}
+
+export default createItem;
