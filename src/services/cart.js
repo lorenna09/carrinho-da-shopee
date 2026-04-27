c9ea4f80f9d9f361ece1e8eeaefa160cac65cbe6
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-27 11:35:34 -0300

    Update cart.js

--- src/services/cart.js
+++ src/services/cart.js
@@ -1,15 +1,26 @@
 // quais açoes meu carinho pode fazer
 
 //CASOS DE USO
-// -> adicionar item no carrinho
-->adicionar item no carrinho
-async function addItem(userCart){}
+
+//->adicionar item no carrinho
+async function addItem(userCart, item){
+   userCart.push(item);
+}
 
 //-> deletar item do carrinho
-async function removeItem(userCart,name){}
+async function removeItem(userCart,name){
+  return userCart.reduce((total, item) => total + item.subtotal(), 0);  
+}
 
 //-> remover um item - diminui um item
 async function removeItem(userCart,index){}
 
 //-> calcular o total do carrinho
-async function addItem(userCart){} 
+async function calculateTotal(userCart){} 
+
+export{
+addIte,
+calculateTotal,
+removeItem,
+removeItem,
+}
