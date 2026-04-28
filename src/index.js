a1a62e15f1c5311b4d7ad9b13ae485d8518a6c4b
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-28 12:12:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as cartService from "./services/cart.js";
 import createItem from "./services/item";
 
 const cart = [];
+const myWhishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
@@ -10,10 +11,10 @@ const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item1 = await createItem("hotwheels lamborghini", 39.99, 3);
 
 await carService.addItem(myCart, item1);
-await carService.addItem(myCart, item2);
+await carService.addItem(myWhishList, item2);
 
 console.log("Shopee Cart TOTAL IS:");
-await cartService.calcuteTotal();
+await cartService.calcuteTotal(myCart);
 
 
 
