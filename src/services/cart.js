da0aef2e83fb01e3672c50d15ff6d5ebccc4288e
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-28 12:17:56 -0300

    Update cart.js

--- src/services/cart.js
+++ src/services/cart.js
@@ -15,7 +15,11 @@ async function calculateTotal(userCart){
 
 //-> deletar item do carrinho
 async function deleteItem(userCart,name){
- 
+   const index = userCart.findIndex((item) => item,name === name);
+
+   if(index !== -1){
+      userCart.splice(index, 1);
+   }
 }
 
 //-> remover um item - diminui um item
