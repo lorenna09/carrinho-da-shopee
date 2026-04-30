22ca33712f034b3c8368677d3de34a3f367a3957
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-30 12:55:29 -0300

    Update item.js

--- src/services/item.js
+++ src/services/item.js
@@ -1,1 +1,12 @@
+// CASOS DE USO DOS ITENS
 
+//-> criar item com subtotal certo 
+async function createItem(name, price, quantily){
+  return {
+    name,
+    price,
+    quantily,
+    subtotal: ()=> price * quantily,
+  };
+}
+export default createItem;
