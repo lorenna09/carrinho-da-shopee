4992d5bce64bd3d4d63d70557674a7809faea1de
lorenna09 <167568325+lorenna09@users.noreply.github.com>
2026-04-30 12:49:19 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 import * as cartService from "./services/cart.js";
 import createItem from "./services/item";
 
-const cart = [];
+const myCart = [];
 const myWhishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
+//criando dois itens
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item1 = await createItem("hotwheels lamborghini", 39.99, 3);
 
@@ -14,12 +15,14 @@ const item1 = await createItem("hotwheels lamborghini", 39.99, 3);
 await carService.addItem(myCart, item1);
 await carService.addItem(myCart, item2);
 
+await carService.removeItem(myCart,item2);
+await carService.removeItem(myCart,item2);
+await carService.removeItem(myCart,item2);
+
+await carService.displaycart(myCart);
 //deletei dois itens do carrinho
-await cartService.deleteItem(myCart, item2.name);
-await cartService.deleteItem(myCart, item1.name);
-
-
-console.log("Shopee Cart TOTAL IS:");
+//await cartService.deleteItem(myCart, item2.name);
+//await cartService.deleteItem(myCart, item1.name);
 await cartService.calcuteTotal(myCart);
 
 
